d8f20c1864164fe3c40ef41990c350e402615076
Dalia Raafat <daliaraafat03@gmail.com>
2024-02-12 00:42:22 +0200

    Base

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import stramlit as st
+import streamlit as st
 
-st.header('Globalize Text')
+
+st.set_page_config(page_title="EntertainerGPT", page_icon=":robot:", layout="centered")
+st.header('Hi, I\'m your EntertainerGPT!')
+
+input_text = st.text_area(label="What kind of movie are you in the mood for?", placeholder="Movie Genre...",  max_chars=20)
+
